ef9480c326c7cba92cde6506c370220059b008bf
phatdroid94 <phatdroid94@gmail.com>
2020-05-09 20:10:32 +0700

    init

---
+++ app/src/main/java/com/example/foregroundservice25022020/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.foregroundservice25022020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
