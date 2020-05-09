d99f6986e7d218b464ecd80cd15542194767cedd
phatdroid94 <phatdroid94@gmail.com>
2020-05-09 20:19:32 +0700

    setup service

--- app/src/main/java/com/example/foregroundservice25022020/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice25022020/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnStart,mBtnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnStart = findViewById(R.id.buttonStartService);
+        mBtnStop = findViewById(R.id.buttonStopService);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
